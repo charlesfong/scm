54b991dfb98a7b4b8822ab251b33824cc60c96aa
odilia12 <40134338+odilia12@users.noreply.github.com>
2019-04-03 18:04:31 +0700

    liaModelMesin

---
+++ app/Mesin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Mesin extends Model
+{
+    use SoftDeletes;
+    protected $table = 'mesin';
+    protected $primaryKey='id_mesin';
+
+    public $timestamps = false; //artinya ndak da created_at sm updated_at
+    protected $fillable = ['nama'];
+
+}
