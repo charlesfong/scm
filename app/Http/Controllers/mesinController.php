1afb73928aca80a6ba84546929368400caad7586
odilia12 <40134338+odilia12@users.noreply.github.com>
2019-04-08 15:22:11 +0700

    ok

--- app/Http/Controllers/mesinController.php
+++ app/Http/Controllers/mesinController.php
@@ -3,68 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\KaryawanFormRequest;
 use App\Mesin;
-use Illuminate\Support\Facades\File;
-class mesinController extends Controller
+
+class MesinController extends Controller
 {
-    // public function create()
-    // {
-    //     return view('mesin.inputmesin');
-    // }
-    // public function store(Request $request)
-    // {
-    //     $mesin = new Mesin;
-    //     $mesin->nama = $request->nama_mesin;
-
-    //     $mesin->save();
-
-    //     return redirect(route('mesin'));
-    // }
-    // public function edit()
-    // {
-    //     $mesin = Mesin::all();
-
-    //     return view('mesin.updatemesin',compact('mesin'));
-    // }
-    // public function update(Request $request)
-    // {
-    //     $array_id="";
-    //     foreach ($request->all() as $key => $value) {
-    //         if ($key!="_token")
-    //         {
-    //             $mesin = Mesin::find($key);
-    //             $mesin->nama=$value;
-    //             $mesin->save();
-    //         }
-    //     }
-    //     return redirect(route('mesin'));
-    // }
-    // public function destroy($id)
-    // {
-    //     $mesin = Mesin::where('id_mesin',$id)->first();
-    //     $mesin->delete();
-    //     return redirect(route('mesin'));
-    // }
-
     public function index() {
         $mesins = Mesin::where('active',1)->get();
         return view('mesin',compact('mesins'));
     }
     public function storemesin(request $request) {
-        $mesin = new Mesin();
+    	$mesin = new Mesin();
         $mesin->nama = $request->nama_mesin;
-        $mesin->tanggal_beli = $request->tgl_beli;
-        if ($request->has('image')){
-            $file = $request->file('image');
-            $filename = $file->getClientOriginalName();
-            $mesin->image = $filename;
-            if (!is_dir("sources/mesin")) {
-                File::makeDirectory("sources/mesin", $mode = 0777, true, true);
-            }
-
-            $pathForImage = "sources/mesin";
-            $file->move($pathForImage, $filename);
-        }
         $mesin->save();
         return redirect(route('showallmesin'));
     }
@@ -85,8 +36,8 @@ class mesinController extends Controller
     {
         $mesin = Mesin::find($request->id_mesin);
         $mesin->nama = $request->nama;
-        $mesin->tanggal_beli = $request->tgl_beli;
         $mesin->save();
         return redirect(route("showallmesin"));
+
     }
 }
